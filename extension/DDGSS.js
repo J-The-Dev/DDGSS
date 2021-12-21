ecdaf630beeca49deb1ba94e7b790cd24a9bfef2
J Dev <jlozier99@gmail.com>
2021-12-21 17:48:04 -0600

    Fix Bugs

--- extension/DDGSS.js
+++ extension/DDGSS.js
@@ -1,5 +1,4 @@
 const rewriteCookies = (settings) => {
-    for (el of document.cookie.split("; ")) {document.cookie = el.split("=")[0] + "=;expires=Thu, 21 Sep 1979 00:00:01 UTC;"}; //Not very clean, but only was to easily remove cookies
     for (el of settings.split("; ")) {document.cookie = el}
 }
 
@@ -7,7 +6,9 @@ chrome.storage.local.get(["DDG-Settings"], (settings) => {
     settings["DDG-Settings"] != "" ? rewriteCookies(settings["DDG-Settings"]) : "";
 })
 
-window.addEventListener('beforeunload', () => {
+const onexit = () => {
     const setSettings = {"DDG-Settings" : document.cookie};
     chrome.storage.local.set(setSettings);
-});
+}
+
+window.onbeforeunload = onexit;
